f0cde614b453e75f617d6f41d0f7afa02f1f47a3
duydt1294 <duydt1294@gmail.com>
2025-03-11 00:12:31 +0700

    Thư viện Util

---
+++ UTIL_SOF205/AuthUtil.cs
@@ -0,0 +1,32 @@
+﻿using DTO_SOF205;
+namespace GUI_SOF205
+{
+    public class AuthUtil
+    {
+        public static NhanVien? user = null;
+
+        /// <summary>
+        /// Đăng xuất người dùng hiện tại.
+        /// </summary>
+        public static void Logout()
+        {
+            user = null;
+        }
+
+        /// <summary>
+        /// Kiểm tra trạng thái đăng nhập.
+        /// </summary>
+        public static Boolean IsLogin()
+        {
+            return user != null;
+        }
+
+        /// <summary>
+        /// Kiểm tra quyền quản lý của tài khoản.
+        /// </summary>
+        public static Boolean IsManager()
+        {
+            return user != null && user.QuanLy;
+        }
+    }
+}
