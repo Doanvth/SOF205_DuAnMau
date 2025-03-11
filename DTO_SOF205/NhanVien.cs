6883782583e401234fffbfb7685a69c9bb6c3a05
duydt1294 <duydt1294@gmail.com>
2025-03-11 09:34:45 +0700

    Các lớp tiện ích UTIL Giao diện chính Bộ icon Lớp xử lý DLL

---
+++ DTO_SOF205/NhanVien.cs
@@ -0,0 +1,10 @@
+﻿namespace DTO_SOF205
+{
+    public class NhanVien
+    {
+        public String? MaNhanVien { get; set; }
+        public String? HoTen { get; set; }
+        public String? MaKhau { get; set; }
+        public Boolean QuanLy { get; set; }
+    }
+}
---
+++ GUI_SOF205/MainScreen.cs
@@ -0,0 +1,10 @@
+﻿namespace GUI_SOF205
+{
+    public partial class MainScreen : Form
+    {
+        public MainScreen()
+        {
+            InitializeComponent();
+        }
+    }
+}
