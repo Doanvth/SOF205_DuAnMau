fdfea60637fd0f99840fc532d75d3ee478dd4dca
phtrang1309 <102840558+phtrang1309@users.noreply.github.com>
2025-03-18 16:14:51 +0700

    [Update CRUD] Sản phẩm - Chỉnh sửa nội dung theo ImageUtil
    [CRUD] Sản phẩm - Chỉnh sửa nội dung theo ImageUtil

--- UTIL_SOF205/ImageUtil.cs
+++ UTIL_SOF205/ImageUtil.cs
@@ -25,37 +25,51 @@ namespace UTIL_SOF205
         {
             string sourceFilePath = openFileDialog.FileName;
 
+            // Kiểm tra file có phải là hình ảnh không trước khi lưu
+            if (!IsImage(openFileDialog))
+            {
+                throw new Exception("File không phải là hình ảnh hợp lệ!");
+            }
+
             string projectFolder = Path.Combine(Application.StartupPath, "Images");
 
             if (!Directory.Exists(projectFolder))
             {
                 Directory.CreateDirectory(projectFolder);
             }
-            //Lấy ngày tháng và thời gian hiện tại sử dụng cho tên ảnh
-            string fileExtension = Path.GetExtension(sourceFilePath);
+
+            // Lấy ngày tháng và thời gian hiện tại sử dụng cho tên ảnh
+            string fileExtension = Path.GetExtension(sourceFilePath).ToLower();
             string fileName = DateTime.Now.ToString("yyyyMMddHHmmss") + fileExtension;
             string destinationFilePath = Path.Combine(projectFolder, fileName);
 
-            //Trả kết quả là tên file đã lưu
+            // Thực hiện sao chép file
             try
             {
                 File.Copy(sourceFilePath, destinationFilePath, true);
-                return fileName;
+                return fileName; // Trả về tên file đã lưu
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-                throw;
+                throw new Exception("Lỗi khi lưu ảnh: " + ex.Message);
             }
         }
-
         /// <summary>
         /// Đọc ảnh được chọn từ thư mục Images
         /// </summary>
         /// <param name="fileName">Tên file cần đọc</param>
         /// <returns>Bitmap file ảnh đã đọc được</returns>
         public Bitmap load(string fileName) {
-            string projectFolder = Path.Combine(Application.StartupPath, "Images");
-            return new Bitmap(Path.Combine(projectFolder, fileName));
+            try
+            {
+                string projectFolder = Path.Combine(Application.StartupPath, "Images");
+                return new Bitmap(Path.Combine(projectFolder, fileName));
+            }
+            catch
+            {
+                return null;
+            }
+           
         }
     }
 }
